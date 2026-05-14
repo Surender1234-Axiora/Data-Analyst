ce0eba528c5f683928df1ef2e615ca2c9856c156
Surender1234-Axiora <surender.v@axioraglobalsolutions.com>
2026-05-14 21:28:54 +0530

    Add files via upload
    [Query optimisation: EXPLAIN, indexes, CTEs vs subqueries]

---
+++ Query optimisation EXPLAIN, indexes, CTEs vs subqueries.sql
@@ -0,0 +1,15 @@
+--------Create Index
+CREATE NONCLUSTERED INDEX IX_Sales_CustomerID
+ON Sales.SalesOrderHeader(CustomerID);
+
+SELECT * FROM Sales.SalesOrderHeader
+WHERE CustomerID = 11000;
+
+-----CTE vs Subquery
+
+SELECT *FROM(
+SELECT CustomerID, SUM(TotalDue) AS TotalSales
+FROM Sales.SalesOrderHeader
+GROUP BY CustomerID) AS SalesSummary
+WHERE TotalSales > 100000;
+
